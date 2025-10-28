f4fbba4b727e86f969bf12c49d15d107cc823718
BaptisteLucas <lucasbaptiste@hotmail.fr>
2025-10-28 18:45:28 +0100

    code optimization

--- RGB_PWM/RGB_PWM.ino
+++ RGB_PWM/RGB_PWM.ino
@@ -12,24 +12,20 @@ void setup() {
   digitalWrite(BLUE, LOW);
 }
 
-// define variables
-int redValue = 255;
-int greenValue = 0;
-int blueValue = 0;
-
 /**
  * Fonction pour faire un fondu entre deux couleurs.
- * @param decreasingColor: Pointeur vers la variable de la couleur à diminuer.
- * @param increasingColor: Pointeur vers la variable de la couleur à augmenter.
  * @param decreasingPin: Broche de la couleur à diminuer.
  * @param increasingPin: Broche de la couleur à augmenter.
  */
-void fadeBetweenColors(int *decreasingColor, int *increasingColor, int decreasingPin, int increasingPin) {
+void fadeBetweenColors(int decreasingPin, int increasingPin) {
+  int decreasingColor = 255;
+  int increasingColor = 0;
+  
   for (int i = 0; i < 255; i += 1) {
-    *decreasingColor -= 1;
-    *increasingColor += 1;
-    analogWrite(decreasingPin, *decreasingColor);
-    analogWrite(increasingPin, *increasingColor);
+    decreasingColor -= 1;
+    increasingColor += 1;
+    analogWrite(decreasingPin, decreasingColor);
+    analogWrite(increasingPin, increasingColor);
     delay(delayTime);
   }
 }
@@ -38,12 +34,12 @@ void fadeBetweenColors(int *decreasingColor, int *increasingColor, int decreasin
 void loop()
 {
   // Fondu du rouge vers le vert
-  fadeBetweenColors(&redValue, &greenValue, RED, GREEN);
+  fadeBetweenColors(RED, GREEN);
 
   // Fondu du vert vers le bleu
-  fadeBetweenColors(&greenValue, &blueValue, GREEN, BLUE);
+  fadeBetweenColors(GREEN, BLUE);
 
   // Fondu du bleu vers le rouge
-  fadeBetweenColors(&blueValue, &redValue, BLUE, RED);
+  fadeBetweenColors(BLUE, RED);
 }
 

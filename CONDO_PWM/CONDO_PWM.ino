8cda741f299cd28b0a80549d684332b5cc76233f
BaptisteLucas <lucasbaptiste@hotmail.fr>
2025-11-18 14:29:29 +0100

    first commit -> to test

---
+++ CONDO_PWM/CONDO_PWM.ino
@@ -0,0 +1,54 @@
+// Configuration des broches
+#define PWM_OUT 3       // Broche PWM pour charger le condensateur
+#define CONDO_INPUT A0  // Broche analogique pour mesurer la tension
+#define CMD_DECH 2      // Broche numérique pour commander la décharge
+#define delayTime 1    // loop delay time in milliseconds
+
+
+int PWMRate = 25;       // Rapport cycle de charge PWM (0-100%)
+float seuilCharge = 2.5; // Seuil de tension (en volts) où la charge est terminée
+
+void setup() {
+  pinMode(CMD_DECH, OUTPUT);  // Configure la broche de décharge en sortie
+  digitalWrite(CMD_DECH, LOW); // Désactive la décharge
+
+  pinMode(PWM_OUT, OUTPUT);   // Configure la broche PWM en sortie
+  analogWrite(PWM_OUT, PWMRate);  // Charge le condensateur avec le rapport cyclique défini
+
+  Serial.begin(9600);         // Initialisation du port série
+}
+
+/**
+ * Fonction qui lit une entrée analogique et retourne la tension en volts.
+ * @param pin: Broche analogique à lire.
+ */
+float voltageReader(int pin) {
+  int valeurAnalogique = analogRead(pin);
+  return valeurAnalogique * (5.0 / 1023.0);  // Conversion en volts (résolution 10 bits)
+                                                      // Le 5.0 permet d'avoir un résultat en float
+}
+
+// main loop
+void loop() {
+  
+  digitalWrite(CMD_DECH, LOW); // Désactive la décharge
+    
+  // Lecture de la tension du condensateur en volts
+  float tension = voltageReader(CONDO_INPUT);
+
+  // Affichage de la tension
+  Serial.print("Tension : ");
+  Serial.print(tension);
+  Serial.println(" V");
+
+
+  // Gestion de la charge
+  if (tension > seuilCharge) {
+    // le condensateur est chargé
+    analogWrite(PWM_OUT, 0); // Arrête la charge
+    digitalWrite(CMD_DECH, HIGH);   // On décharge
+    Serial.print("Ordre de décharge envoyé.\n");
+  }
+
+  delay(delayTime);  // Délai avant de reboucler  
+}
---
+++ CONDO_PWM/README.md
@@ -0,0 +1,7 @@
+TP mettant en oeuvre un condensateur chargé par un signal PWM.
+La décharge est assurée par un transistor 2N2222 :
+- Utilisation de la sortie Arduino PWM 3. Câblage d'une diode + résistance 10k + condensateur 1µF en série.
+- Câblage en parallèle du condensateur d'une résistance 10 Ohms + transistor 2N2222
+- Pilotage du transistor sur la sortie x
+- Acquisition de la charge du condensateur sur l'entrée y
+
